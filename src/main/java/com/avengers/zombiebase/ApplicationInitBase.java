b40491aa634664b2d1f3f6e78a98889341119482
shejian <shejian@taiwu.com>
2018-06-15 14:51:19 +0800

    ARouter 的基础调用

---
+++ src/main/java/com/avengers/zombiebase/ApplicationInitBase.java
@@ -0,0 +1,32 @@
+package com.avengers.zombiebase;
+
+import android.app.Application;
+
+import com.alibaba.android.arouter.launcher.ARouter;
+import com.avengers.zombielibrary.BuildConfig;
+
+/**
+ * @author jvis
+ * @date 2018-06-15
+ */
+public class ApplicationInitBase {
+
+    /**
+     * 初始化ARouter
+     *
+     * @param app Application
+     */
+    public static void initARouter(Application app) {
+        // 这两行必须写在init之前，否则这些配置在init过程中将无效
+        if (BuildConfig.DEBUG) {
+            // 打印日志
+            ARouter.openLog();
+            // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
+            ARouter.openDebug();
+        }
+        // 尽可能早，推荐在Application中初始化
+        ARouter.init(app);
+    }
+
+
+}
--- src/main/java/com/avengers/zombiebase/ZombieBaseUtils.java
+++ src/main/java/com/avengers/zombiebase/ZombieBaseUtils.java
@@ -2,9 +2,12 @@ package com.avengers.zombiebase;
 
 
 import android.content.Context;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
+import com.alibaba.android.arouter.facade.Postcard;
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.avengers.zombielibrary.BuildConfig;
 import com.spinytech.macore.MaActionResult;
 import com.spinytech.macore.MaApplication;
@@ -29,6 +32,19 @@ public class ZombieBaseUtils {
 
     public static RouterResponse onLocalRoute(Context context, @NonNull RouterRequest routerRequest) throws Exception {
         return getLocalRoute().route(context, routerRequest);
+    }
+
+
+    public static void startNavigation(String routerPath, Bundle bundle) {
+        ARouter.getInstance().build(routerPath).with(bundle).navigation();
+    }
+
+    public static void startNavigation(String routerPath) {
+        startNavigation(routerPath, null);
+    }
+
+    public static Postcard startNavigationBuild(String routerPath) {
+        return ARouter.getInstance().build(routerPath);
     }
 
 
