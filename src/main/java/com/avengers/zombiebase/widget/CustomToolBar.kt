d64327045c6b0082d72c681ba468ffe71d0b4faf
shejian <shejian@taiwu.com>
2018-07-20 18:39:50 +0800

    集成toolbar

--- src/main/java/com/avengers/zombiebase/widget/CustomToolBar.kt
+++ src/main/java/com/avengers/zombiebase/widget/CustomToolBar.kt
@@ -1,8 +1,13 @@
 package com.avengers.zombiebase.widget
 
 import android.content.Context
+import android.graphics.Color
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.Toolbar
 import android.util.AttributeSet
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.TextView
 import com.avengers.zombielibrary.R
 
@@ -13,6 +18,21 @@ class CustomToolBar : Toolbar {
     constructor(context: Context, attributes: AttributeSet, defaultAttr: Int) : super(context, attributes, defaultAttr)
 
     lateinit var mTxtMiddleTitle: TextView
+
+    init {
+        initView()
+    }
+
+    private fun initView() {
+        setNavigationIcon(R.drawable.ic_arrow_back_black_24dp)
+        setTitleTextColor(ContextCompat.getColor(context, R.color.black_87_color))
+        setBackgroundColor(Color.WHITE)
+        mTxtMiddleTitle = View.inflate(context, R.layout.toolbar_text_view, null) as TextView
+        val vp = LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        vp.gravity = Gravity.CENTER
+        addView(mTxtMiddleTitle, vp)
+    }
+
 
     override fun onFinishInflate() {
         super.onFinishInflate()
