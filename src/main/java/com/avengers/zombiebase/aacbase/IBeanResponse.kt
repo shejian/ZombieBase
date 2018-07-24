fcf790000225281bfe5fc88794951d61537a42f0
cd-butterfly <cd_butterfly@qq.com>
2018-07-24 16:59:14 +0800

    还原部分写法。等后期待定

--- src/main/java/com/avengers/zombiebase/aacbase/IBeanResponse.kt
+++ src/main/java/com/avengers/zombiebase/aacbase/IBeanResponse.kt
@@ -1,17 +1,13 @@
 package com.avengers.zombiebase.aacbase
 
-import com.bty.retrofit.net.bean.JsonBeanRequest
+import java.io.Serializable
 
 /**
  * 用来对主要的操作数据做标记
  */
-open class IBeanResponse : JsonBeanRequest() {
-
-    var message: String = ""
-    var status: Int = 0
-}
+interface IBeanResponse : Serializable
 
 /**
  * 请求参数必须实现这个接口，用于标记是请求参数
  */
-open class IReqParam : JsonBeanRequest()
+interface IReqParam : Serializable
