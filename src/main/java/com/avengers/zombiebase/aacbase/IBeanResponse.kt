6a061645c64fa3f595f8efe72ba6291b9cce750e
cd-butterfly <cd_butterfly@qq.com>
2018-07-24 15:40:55 +0800

    aac 框架整理完善

--- src/main/java/com/avengers/zombiebase/aacbase/IBeanResponse.kt
+++ src/main/java/com/avengers/zombiebase/aacbase/IBeanResponse.kt
@@ -1,13 +1,17 @@
 package com.avengers.zombiebase.aacbase
 
-import java.io.Serializable
+import com.bty.retrofit.net.bean.JsonBeanRequest
 
 /**
  * 用来对主要的操作数据做标记
  */
-interface IBeanResponse : Serializable
+open class IBeanResponse : JsonBeanRequest() {
+
+    var message: String = ""
+    var status: Int = 0
+}
 
 /**
  * 请求参数必须实现这个接口，用于标记是请求参数
  */
-interface IReqParam : Serializable
+open class IReqParam : JsonBeanRequest()
